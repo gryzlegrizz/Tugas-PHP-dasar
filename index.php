74ac03e6abee713276a73efe7167160f4e8822a2
Gilang Andhika <114631106+gryzlegrizz@users.noreply.github.com>
2022-12-15 16:29:00 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ini Latihan PHP Dasar</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis voluptate nostrum laudantium ipsa vitae voluptates officiis consequuntur numquam quos. Perferendis!</p>
+    <br>
+    <!-- ini adalah kodingan php -->
+    <?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?>
+
+</body>
+</html>
