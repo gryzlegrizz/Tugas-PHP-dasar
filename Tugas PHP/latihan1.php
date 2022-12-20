e3058d0ebbe142e3e55afb17d9029109a1779f93
Gilang Andhika <114631106+gryzlegrizz@users.noreply.github.com>
2022-12-20 13:10:43 +0700

    Add files via upload

---
+++ Tugas PHP/latihan1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lat 1</title>
+</head>
+<body>
+    <h2>Latihan 1</h2>
+    <?php
+        $bil=7;
+
+        if($bil %2==0){
+            echo "$bil Merupakan Bilangan Genap";
+        }else{
+            echo "$bil Merupakan Bilangan Ganjil";
+        }
+    ?>
+</body>
+</html>
